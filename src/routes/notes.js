34f167d0bbc958cea3471e6452b1020b50c02e32
RodrigoFA216 <rfloresa1900@alumno.ipn.mx>
2022-05-27 02:03:16 -0500

    Pre connect flash, crud listo

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,4 +1,5 @@
 const express= require('express');
+const async = require('hbs/lib/async');
 const router= express.Router();
 
 const Note= require ('../models/Note');
@@ -35,6 +36,20 @@ router.get('/notes', async(req, res)=>{
     res.render('notes/all-notes', {notes});
 })
 
+router.get('/notes/edit/:id', async (req,res)=>{
+    const note= await Note.findById(req.params.id);
+    res.render('notes/edit-note', {note});
+});
 
+router.put('/notes/edit-note/:id', async(req, res)=>{
+    const {title, description}=req.body;
+    await Note.findByIdAndUpdate(req.params.id, {title,description});
+    res.redirect('/notes');
+});
+
+router.delete('/notes/delete/:id', async (req, res)=>{
+    await Note.findByIdAndDelete(req.params.id);
+    res.redirect('/notes');
+})
 
 module.exports=router;
